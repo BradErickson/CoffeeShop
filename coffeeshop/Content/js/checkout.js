95a6ed0bef5e27de774ad8850752a9d5748a63ad
Brad Erickson <berickson324@gmail.com>
2017-08-10 14:32:15 -0500

    more refactoring to get rid of console errors

--- coffeeshop/Content/js/checkout.js
+++ coffeeshop/Content/js/checkout.js
@@ -9,30 +9,13 @@ var json = {
     "medium mocha": { 'price': '6.75', 'name': 'Medium Mocha' },
     "large mocha": { 'price': '7.25', 'name': 'Large Mocha' },
 }
-function addToCart(item) {
-    
-    $('#myModal').modal();
-    $(".itemAdded").remove();
-    $('.modal-body').append('<p class="itemAdded" style="text-align:center"> A ' + item + " has been successfully added to your shopping cart</p>");
-    shoppingCartItems = [];
-    shoppingCartItems.push(item);
-    var selected = json[shoppingCartItems[0]];
-    
-    if (sessionStorage.getItem(json[shoppingCartItems[0]].name)) {
-        var quan = parseInt(sessionStorage.getItem(json[shoppingCartItems[0]].name).split(',')[2]);
-        quan++;
-        sessionStorage.setItem(json[shoppingCartItems[0]].name, [json[shoppingCartItems[0]].name, json[shoppingCartItems[0]].price, quan]);
-    } else {
-        sessionStorage.setItem(json[shoppingCartItems[0]].name, [json[shoppingCartItems[0]].name, json[shoppingCartItems[0]].price, 1]);
-    }
-    
-}
+
 $(document).ready(function () {
     getItemsFromSession();
 });
 function getItemsFromSession() {
     var session = {};
-    
+
     for (var i in json) {
         var ses = sessionStorage.getItem(json[i].name);
         if (ses) {
@@ -43,7 +26,7 @@ function getItemsFromSession() {
     refreshCart();
 }
 function refreshCart() {
-   var items = this.session
+    var items = this.session
     for (var j in session) {
         $(".shoppingCartItem").append('<div class="col-xs-12"><b>Item: ' + items[j].split(',')[0] + '</b></div><div class="col-xs-12"><b>Each: ' + items[j].split(',')[1] + '</div></b>' + '</b></div><div class="col-xs-12"><b>Quantity: ' + items[j].split(',')[2] + '</div></b><hr>');
     }
@@ -51,7 +34,7 @@ function refreshCart() {
     $("#checkoutTotal").html('<p>Order Total: ' + checkoutTotal + "</p>");
 }
 
-function getTotals(){
+function getTotals() {
     var items = this.session;
     var totals = [];
     for (var j in items) {
